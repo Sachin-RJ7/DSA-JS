85a22593fdc6d165708a50dda6fee7492e01c835
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-06-18 08:27:40 +0500

    find greates common divisior or HCF

---
+++ 1. Problem solving on Loops/gcd.js
@@ -0,0 +1,13 @@
+
+function gcd(a, b){
+    let ans = 1;
+    for(let i = 2; i < Math.min(a, b); i++){
+        if(a % i === 0 && b % i === 0){
+            ans = i;
+        }
+    }
+    return ans;
+}
+
+const ans = gcd(7, 11);
+console.log(ans);
