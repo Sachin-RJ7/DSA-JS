8d7722f592997a37e4219bd5227ccdb48dbe6afa
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-06-29 09:03:49 +0500

    find intersection of two arrays

---
+++ 4. Problem solving on Arrays/intersectionOfTwoArrays.js
@@ -0,0 +1,27 @@
+var intersection = function(nums1, nums2) {
+    let freqNums1 = {};
+    for(let i = 0; i < nums1.length; i++){
+        let element = nums1[i];
+        if(freqNums1[element]){
+            freqNums1[element]++;
+        }
+        else {
+            freqNums1[element] = 1;
+        }
+    }
+
+    let freqNums2 = {}
+    for(let i = 0; i < nums2.length; i++){
+        let element = nums2[i];
+        if(freqNums1[element]){
+            freqNums2[element] = 1;
+        }
+    }
+
+    return Object.keys(freqNums2);
+};
+
+const nums1 = [4,9,5];
+const nums2 = [9,4,9,8,4];
+
+console.log(intersection(nums1, nums2));
